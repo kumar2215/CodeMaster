4b2911f38b3faf7b29fd80dbe49578c39ea4cb75
kumar2215 <kumar@LAPTOP-MR00U8RR>
2024-06-16 23:54:22 +0800

    updated

--- codemaster/app/questionGeneration/uploadTournament.ts
+++ codemaster/app/questionGeneration/uploadTournament.ts
@@ -1,18 +1,36 @@
-"use server"
-
 import { createClient } from '@supabase/supabase-js';
+import fs from 'fs';
 import upload from './upload';
 
+const envFile = fs.readFileSync("./.env.local").toString();
+const lines = envFile.split("\n");
+const NEXT_PUBLIC_SUPABASE_URL = lines[0].split("=")[1];
+const NEXT_PUBLIC_SUPABASE_ANON_KEY = lines[1].split("=")[1];
+const EMAIL = lines[2].split("=")[1];
+const PASSWORD = lines[3].split("=")[1];
 
+const supabase = createClient(
+  NEXT_PUBLIC_SUPABASE_URL,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-
-function formatTimestamptzFromEpoch(epochTimeSeconds: number): string {
+function formatTimestamptzFromEpoch(epochTimeSeconds: number) : string {
     const isoString = new Date(epochTimeSeconds).toISOString();
     return isoString
 }
 
 export async function uploadTournament(questions: any[], deadlineEpoch: number, createdBy: string) {
+
+  if (!questions || questions.length === 0) {
+    throw new Error('No questions found.');
+  }
+
+  const { data, error: err } = await supabase.auth.signInWithPassword({
+    email: EMAIL,
+    password: PASSWORD
+  });
+  if (err) { console.error(err); }
+
   try {
     if (!deadlineEpoch) {
       throw new Error('Deadline is required.');
@@ -20,7 +38,6 @@ export async function uploadTournament(questions: any[], deadlineEpoch: number, 
 
     // Ensure deadline is a valid date string
     const deadlineTimestamptz = formatTimestamptzFromEpoch(deadlineEpoch);
-
 
     const questionIDs = [];
     console.log(questions)
