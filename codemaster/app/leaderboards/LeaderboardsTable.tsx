8c83244e948eb30b1058249d4efc35b19bbed2a1
Ling Ziyan <lingziyan2015@gmail.com>
2024-06-27 20:27:00 +0800

    Leaderboards built

---
+++ codemaster/app/leaderboards/LeaderboardsTable.tsx
@@ -0,0 +1,44 @@
+"use client"
+import React from 'react'
+import { useState, useEffect } from 'react';
+import { LeaderboardsRow } from './LeaderboardsRow';
+
+
+
+export const LeaderboardsTable = ({data, xpType, header}) => {
+
+  return (
+    <div className="min-h-screen h-full flex flex-col items-center pb-14 m-0 w-full">
+      <article className="w-10/12 px-2 ">
+  
+    {/*TABLE */}
+    <div className="w-full overflow-x-scroll md:overflow-auto max-w-7xl 2xl:max-w-none r rounded-tl-xl rounded-tr-xl">
+    <table className="table-fixed  overflow-scroll md:overflow-auto w-full text-left font-inter rounded-lg border-l border-r border-gray-800 ">
+    {/**Table header */}
+      <thead
+        className={` rounded-lg text-base text-white font-semibold w-full border-grey bg-neutral-800 border-b`}
+      >
+        <tr className="bg-[#222E3A]/[6%]  border-grey">
+          <th className="text-xs font-medium px-0 py-4 text-center w-2/12">
+            RANK
+          </th>
+          <th className="text-xs font-medium pl-24 py-4 text-left w-7/12">               
+            USER
+          </th>
+          <th className="text-xs font-medium px-6 py-4 text-center w-3/12">
+            {header}
+        </th>
+        </tr>
+      </thead>
+      <tbody className="border-grey border-gray-700 border-b ">
+          {data.map((data, index) => {
+          
+            return (<LeaderboardsRow data={data} index={index} key={index} xpType={xpType}/>)
+          })}
+        </tbody>
+    </table>
+    </div>
+  </article>
+    </div>
+  )
+}
--- codemaster/app/leaderboards/page.tsx
+++ codemaster/app/leaderboards/page.tsx
@@ -1,6 +1,8 @@
 import Navbar from "@/components/misc/navbar";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import { LeaderboardsTable } from "./LeaderboardsTable";
+import { LeaderboardsPage } from "./LeaderboardsPage";
 
 const thisLink = "/leaderboards";
 
@@ -15,11 +17,44 @@ export default async function ContestsPage() {
     return redirect("/login");
   }
 
+  //Fetching Users from db
+  const { data: userData, error: err } = 
+  await supabase
+  .from("Users")
+  .select('XP, username, tournament_XP, contest_XP')
+  .limit(100)
+
+  let sortedByTournamentXP = []
+  let sortedByContestXP  = []
+  let sortedByTotalXP = []
+
+  if (userData != null) {
+
+    sortedByTournamentXP = [...userData];
+    sortedByTournamentXP.sort((a, b) => (b.tournament_XP) - (a.tournament_XP));
+
+    sortedByContestXP = [...userData];
+    sortedByContestXP.sort((a, b) => (b.contest_XP) - (a.contest_XP));
+
+    sortedByTotalXP = [...userData];
+    sortedByTotalXP.sort((a, b) => {
+      const totalXP_A = (a.XP) + (a.tournament_XP) + (a.contest_XP);
+      const totalXP_B = (b.XP) + (b.tournament_XP) + (b.contest_XP);
+      
+      return totalXP_B - totalXP_A;
+    });
+
+  }
+
+
+  
+  if (err) { console.error(err); }
+
   return (
     <div className="flex-1 w-full flex flex-col gap-10 items-center" style={{backgroundColor: "#80bfff"}}>
       <Navbar thisLink={thisLink} />
-      <div className="text-xl font-bold">
-        <h1>Not implemented yet</h1>
+      <div className="text-xl font-bold w-full">
+        <LeaderboardsPage sortedByTournamentXP={sortedByTournamentXP} sortedByContestXP={sortedByContestXP} sortedByTotalXP={sortedByTotalXP}/>
       </div>
     </div>
   );
