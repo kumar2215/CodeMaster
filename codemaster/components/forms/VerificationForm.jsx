25a61d63d7fa3fd8f4b9993dbdd64faf27211e8f
kumar2215 <kumar@LAPTOP-MR00U8RR>
2024-07-29 01:00:51 +0800

    fixed bug

--- codemaster/components/forms/VerificationForm.jsx
+++ codemaster/components/forms/VerificationForm.jsx
@@ -2,7 +2,6 @@
 import { createClient } from "@/utils/supabase/client";
 import { useForm } from "react-hook-form";
 import { useState, useEffect } from "react";
-import submitVerification from "@/app/utils/Submissions/submitVerification";
 import { SubmitButton } from "@/components/buttons/SubmitButton";
 import { toast } from "react-toastify";
 
@@ -70,11 +69,92 @@ export default function VerificationForm({ username }) {
     uploadFile();
   }, [savedFile]);
 
-  const data = {
-    file,
-    publicFileUrl,
-    uploadedBefore,
-    username
+
+  const submitVerification = async (formData) => {
+    
+    const firstName = formData.firstName;
+    const lastName = formData.lastName;
+    const email = formData.email;
+    const role = formData.role;
+    const otherRole = formData.otherRole;
+    const organizationName = formData.organizationName;
+    
+    // data validation
+    if (!firstName || !lastName || !email || !role || !organizationName) {
+      toast.error("Please fill in all required fields.", {autoClose: 3000});
+      return;
+    }
+    if (role === "Other" && !otherRole) {
+      toast.error("Please fill in all required fields.", {autoClose: 3000});
+      return;
+    }
+    if (!file) {
+      toast.error("Please upload a file.", {autoClose: 3000});
+      return;
+    }
+    if (file && file.size > 2_097_152) {
+      toast.error("File cannot be larger than 2MB.", {autoClose: 3000});
+      return;
+    }
+    if (!publicFileUrl || !uploadedBefore) {
+      toast.error("Something went wrong while uploading the file. Please try again.", {autoClose: 3000});
+      return;
+    }
+
+    const verificationData = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      role: role === "Other" ? otherRole : role,
+      organizationName: organizationName,
+      proofOfRole: {
+        filename: file.name,
+        bucket: "documents",
+        location: `${username}/proofOfRole-${file.name}`,
+        publicUrl: publicFileUrl
+      }
+    };
+
+    const { data, error } = await supabase.from("Verifications")
+      .insert({ user: username, verification_data: verificationData, status : "pending" })
+      .select();
+
+    if (error) {
+      toast.error("Something went wrong. Please try again.", {autoClose: 3000});
+      return;
+    }
+
+    const admins = process.env.NEXT_PUBLIC_ADMINS?.split(",");
+    
+    const notification = {
+      from: username,
+      message: `${username} has applied to become a verified user. Waiting for approval.`,
+      type: "Approve",
+      link: `/verify/${data && data[0].id}`
+    }
+
+    for (const admin of admins) {
+      const res = await supabase.from("Users")
+        .select("notifications")
+        .eq("username", admin);
+
+      if (res.error) { 
+        toast.error("Something went wrong. Please try again.", {autoClose: 3000});
+        return;
+      }
+
+      const notifications = res.data && res.data[0].notifications;
+      notifications.push(notification);
+      const res2 = await supabase.from("Users")
+        .update({ notifications })
+        .eq("username", admin);
+      if (res2.error) { 
+        toast.error("Something went wrong. Please try again.", {autoClose: 3000});
+        return;
+      }
+    }
+
+    toast.success("Verification form has been submitted successfully!", {autoClose: 3000});
   }
 
   return (
@@ -154,7 +234,7 @@ export default function VerificationForm({ username }) {
       
       <div className='flex flex-row w-full gap-3 p-5 ml-2'>
         <SubmitButton
-          formAction={formData => handleSubmit(submitVerification(formData, data))}
+          formAction={handleSubmit(submitVerification)}
           className='btn btn-success'
           pendingText='Submitting...'
         >
